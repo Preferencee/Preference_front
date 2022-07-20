482fb2680e5aa7dc8b243ee1df523e4f01bd669c
KMS <80440158+ddolly518@users.noreply.github.com>
2022-07-20 22:20:42 +0900

    volley오류고치기

--- app/src/main/java/com/example/preference/MainActivity.java
+++ app/src/main/java/com/example/preference/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.preference;
-import androidx.appcompat.app.AppCompatActivity;
-import android.graphics.Paint;
-import android.os.Bundle;
-import android.text.Html;
-import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
