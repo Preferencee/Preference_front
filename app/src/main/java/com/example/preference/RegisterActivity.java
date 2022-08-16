e0fafd55c6db63125099e0f637041d31fd3f3077
KMS <80440158+ddolly518@users.noreply.github.com>
2022-08-16 20:53:42 +0900

    회원가입

--- app/src/main/java/com/example/preference/RegisterActivity.java
+++ app/src/main/java/com/example/preference/RegisterActivity.java
@@ -3,12 +3,20 @@ package com.example.preference;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    ChipGroup chipGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
     }
 }
